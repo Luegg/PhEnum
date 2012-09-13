3dcaeaeb9e6eae31e039f6d9848442ca3b16b618
Luegg <wegmaluk@gmail.com>
2012-09-13 20:10:09 +0200

    spaces instead of tabs

--- Enum.php
+++ Enum.php
@@ -3,116 +3,116 @@
 namespace Luegg\PhEnum;
 
 function define($name, $types){
-    return Enum::define($name, $types);
+  return Enum::define($name, $types);
 }
 
 class Enum{
 
-    private static $classCode = <<<'EOT'
+  private static $classCode = <<<'EOT'
 <namespace>
 
 use Luegg\PhEnum\Enum;
 
 function <enumName>($ordinal){
-    return <enumName>::lookup($ordinal);
+  return <enumName>::lookup($ordinal);
 }
 
 class <enumName> extends Enum{
-    private static $map = array();
+  private static $map = array();
 
-    private $ordinal;
+  private $ordinal;
 
-    private $name;
+  private $name;
 
-    private function __construct($ordinal, $name){
-        $this->ordinal = $ordinal;
-        $this->name = $name;
-    }
+  private function __construct($ordinal, $name){
+    $this->ordinal = $ordinal;
+    $this->name = $name;
+  }
 
-    static function init(){
-        <init>
-    }
+  static function init(){
+    <init>
+  }
 
-    function __toString(){
-        return $this->name;
-    }
+  function __toString(){
+    return $this->name;
+  }
 
-    function name(){
-        return $this->name;
-    }
+  function name(){
+    return $this->name;
+  }
 
-    function ordinal(){
-        return $this->ordinal;
-    }
+  function ordinal(){
+    return $this->ordinal;
+  }
 
-    static function lookup($ordinal){
-        return self::$map[$ordinal];
-    }
+  static function lookup($ordinal){
+    return self::$map[$ordinal];
+  }
 
-    <methods>
+  <methods>
 }
 
 <enumName>::init();
 EOT;
 
-    private static $initCode = <<<'EOT'
-        self::$map[<ordinal>] = new <enumName>(<ordinal>, "<typeName>");
+  private static $initCode = <<<'EOT'
+    self::$map[<ordinal>] = new <enumName>(<ordinal>, "<typeName>");
 EOT;
 
-    private static $methodCode = <<<'EOT'
-    static function <typeName>(){
-        return self::$map[<ordinal>];
-    }
+  private static $methodCode = <<<'EOT'
+  static function <typeName>(){
+    return self::$map[<ordinal>];
+  }
 EOT;
 
-    static function define($enumName, $typeNames){
-        $parts = explode("\\", $enumName);
-        $enumName = array_pop($parts);
-        $namespace = implode("\\", $parts);
+  static function define($enumName, $typeNames){
+    $parts = explode("\\", $enumName);
+    $enumName = array_pop($parts);
+    $namespace = implode("\\", $parts);
 
-        foreach ($typeNames as $key => $typeName) {
-            $types[] = array(
-                    'ordinal' => $key,
-                    'name' => $typeName,
-                );
-        }
-
-        $inits = self::createInits($enumName, $types);
-        $methods = self::createMethods($enumName, $types);
-        $class = self::createClass($namespace, $enumName, $inits, $methods);
-
-        eval($class);
+    foreach ($typeNames as $key => $typeName) {
+      $types[] = array(
+          'ordinal' => $key,
+          'name' => $typeName,
+        );
     }
 
-    private static function createInits($enumName, $types){
-        $code = self::$initCode;
-        return array_map(function($type) use ($enumName, $code){
-                return str_replace(
-                        array("<typeName>", "<ordinal>", "<enumName>"),
-                        array($type['name'], $type['ordinal'], $enumName),
-                        $code
-                    );
-            }, $types);
-    }
+    $inits = self::createInits($enumName, $types);
+    $methods = self::createMethods($enumName, $types);
+    $class = self::createClass($namespace, $enumName, $inits, $methods);
 
-    private static function createMethods($enumName, $types){
-        $code = self::$methodCode;
-        return array_map(function($type) use ($enumName, $code){
-                return str_replace(
-                        array("<typeName>", "<ordinal>", "<enumName>"),
-                        array($type['name'], $type['ordinal'], $enumName),
-                        $code
-                    );
-            }, $types);
-    }
+    eval($class);
+  }
 
-    private static function createClass($namespace, $enumName, $inits, $methods){
-        if($namespace)
-            $namespace = sprintf("namespace %s;", $namespace);
+  private static function createInits($enumName, $types){
+    $code = self::$initCode;
+    return array_map(function($type) use ($enumName, $code){
         return str_replace(
-                array("<namespace>", "<enumName>", "<init>", "<methods>"),
-                array($namespace, $enumName, implode(PHP_EOL, $inits), implode(PHP_EOL, $methods)),
-                self::$classCode
-            );
-    }
+            array("<typeName>", "<ordinal>", "<enumName>"),
+            array($type['name'], $type['ordinal'], $enumName),
+            $code
+          );
+      }, $types);
+  }
+
+  private static function createMethods($enumName, $types){
+    $code = self::$methodCode;
+    return array_map(function($type) use ($enumName, $code){
+        return str_replace(
+            array("<typeName>", "<ordinal>", "<enumName>"),
+            array($type['name'], $type['ordinal'], $enumName),
+            $code
+          );
+      }, $types);
+  }
+
+  private static function createClass($namespace, $enumName, $inits, $methods){
+    if($namespace)
+      $namespace = sprintf("namespace %s;", $namespace);
+    return str_replace(
+        array("<namespace>", "<enumName>", "<init>", "<methods>"),
+        array($namespace, $enumName, implode(PHP_EOL, $inits), implode(PHP_EOL, $methods)),
+        self::$classCode
+      );
+  }
 }
--- exampleDeclaration.php
+++ exampleDeclaration.php
@@ -3,8 +3,8 @@ require 'Enum.php';
 
 // Note: You have to escape the namespace delimiters
 Luegg\PhEnum\define(
-        'yourCompany\\awesomeApp\\Season',
-        array('Spring', 'Summer', 'Fall', 'Winter')
-    );
+    'yourCompany\\awesomeApp\\Season',
+    array('Spring', 'Summer', 'Fall', 'Winter')
+  );
 
 ?>
