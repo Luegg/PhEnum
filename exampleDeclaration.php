056d1f882e9ddce61468e62e062acbbf9632261b
Luegg <wegmaluk@gmail.com>
2012-09-13 19:42:03 +0200

    example added

---
+++ exampleDeclaration.php
@@ -0,0 +1,7 @@
+<?php
+require 'Enum.php';
+
+// Note: You have to escape the namespace delimiters
+Luegg\PhEnum\define('yourCompany\\awesomeApp\\Season', array('Spring', 'Summer', 'Fall', 'Winter'));
+
+?>
